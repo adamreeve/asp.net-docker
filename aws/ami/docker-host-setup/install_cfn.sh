96f86bf1624c28124321b68bb4d0230a38a1ae32
Adam Reeve <adreeve@gmail.com>
2015-05-24 16:43:54 +1200

    Run cnf-hup as systemd unit in base docker AMI

--- aws/ami/docker-host-setup/install_cfn.sh
+++ aws/ami/docker-host-setup/install_cfn.sh
@@ -3,3 +3,7 @@
 # Install Cloud Formation helper scripts
 sudo yum -y install python-setuptools
 sudo easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+
+sudo cp /tmp/cfn-hup.service /etc/systemd/system/cfn-hup.service
+
+sudo systemctl enable cfn-hup.service
