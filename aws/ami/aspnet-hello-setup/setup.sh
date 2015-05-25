806990e821754df1670d56123e9b54e8e96bd821
Adam Reeve <adreeve@gmail.com>
2015-05-25 20:47:41 +1200

    Release: Update docker image used in AMI

--- aws/ami/aspnet-hello-setup/setup.sh
+++ aws/ami/aspnet-hello-setup/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo docker pull docker.io/adreeve/aspnet-hello:120f95addabb382f96df63d784a24e753b3132a7
+sudo docker pull docker.io/adreeve/aspnet-hello:282952fd7318b5793bbf353778c358ce258af9d6
 
 sudo cp /tmp/aspnet-hello.service /etc/systemd/system/aspnet-hello.service
 
